465e3e4c38bd6a15e7b08f44a76050782c99afbb
Aadesh Sonawane <sonawaneaadesh1524@gmail.com>
2026-07-19 00:59:14 +0530

    Day 007: Learn Python Lists

---
+++ Day-007/challenge.py
@@ -0,0 +1,11 @@
+list = ["FC Barcelona", "Manchester United", "Paris Saint-Germain", "Chelsea", "Manchester City"]
+
+print(list[0])
+print(list[4])
+
+list.append("Real Madrid")
+print(list)
+
+list.pop()
+
+print(list)
---
+++ Day-007/list_basics.py
@@ -0,0 +1,6 @@
+languages = ["Python", "Java", "C", "C++"]
+
+print(languages)
+
+print("First Language:", languages[0])
+print("Last Language:", languages[-1])
